6a02a87b29b0256f30c8b4c54d90d12860534563
Camilo Del Valle Ledesma <camilodelvallel@gmail.com>
2022-09-28 23:57:10 -0500

    Cambio ruta de carpeta promise y fin reto async

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -23,3 +23,27 @@ const anotherFunction = async (urlApi) => {
 }
 
 anotherFunction(API);
+
+/*
+En este desafío vas a conectarte a una API que no existe, por ende debes capturar el error haciendo uso de try/catch y lanzar un error con el mensaje API Not Found.
+
+La solución debería tener un input y output como los siguientes:
+
+Input
+
+await runCode();
+
+Output
+
+Error: API No Found
+*/
+
+async function runCode() {
+    const url = 'https://domain-api-com';
+    try {
+      await fetch(url)
+    } catch (error) {
+      throw new Error('API Not Found');
+    }
+}
+runCode();
